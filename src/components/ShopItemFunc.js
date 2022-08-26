746ef6145647ebbb2c63eb173bce2b72590218a1
bombik815 <58844959+bombik815@users.noreply.github.com>
2022-08-26 13:24:11 +0300

    first commit

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropType from 'prop-types';
+
+export default function ShopItemFunc(props){
+	const { item } = props;
+
+
+	return(
+	<div class="main-content">
+		<h2>{item.brand}</h2>
+		<h1>{item.title}</h1>
+		<h3>{item.description}</h3>
+		<div class="description">{item.descriptionFull}</div>
+		<div class="highlight-window mobile">
+			<div class="highlight-overlay"></div>
+		</div>
+		<div class="divider"></div>
+		<div class="purchase-info">
+			<div className="price">{item.currency + item.price + '.00'}</div>
+			<button>Добавить в корзину</button>
+		</div>
+	</div>
+	)
+}
+
+
+
+
